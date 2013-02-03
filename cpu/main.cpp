69f77146f4b10ceac3c9c5cd148caf8d03df5650
Jun He <junnhe@gmail.com>
2013-02-03 14:44:04 -0800

    added initial files

---
+++ cpu/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/03/2013 02:41:25 PM
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Jun He
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
+
