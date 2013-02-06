0a849c91672324763a8a267da30b77fc943b4bae
Jun He <junnhe@gmail.com>
2013-02-05 16:25:37 -0800

    added runtest.py

---
+++ cpu/runtest.py
@@ -0,0 +1,40 @@
+import itertools,subprocess
+import sys
+from time import gmtime, strftime
+
+exefile = "./cputest.x"
+nthreads = ["1","2","4"]
+optype = ["flops", "iops"]
+nloops = ["1000000", "10000000", "100000000"]
+
+parameters = [nthreads, optype, nloops]
+
+paralist = list(itertools.product(*parameters))
+
+jobid = strftime("%Y-%m-%d-%H-%M-%S", gmtime())
+logname = jobid + ".log"
+resultname = jobid + ".result"
+
+if __name__ == '__main__':
+    logf = open(logname, 'a')
+    resultf = open(resultname, 'a')
+    header = "nthreads optype nloops totaltime opps"
+    resultf.write(header + "\n")
+    print header
+    for para in paralist:
+        mycmd = [exefile] + list(para)
+        proc = subprocess.Popen(mycmd,
+                       stdout=subprocess.PIPE,
+                       stderr=logf)
+        proc.wait()
+        for line in proc.stdout:
+            if 'GREPMARKER' in line:
+                parastr = ''.join(str(e)+" " for e in list(para))
+                perf = line.split()[1:3]
+                perfstr = ''.join(str(e)+" " for e in perf)
+                print parastr, perfstr
+                resultf.write( parastr + perfstr + "\n" ) 
+                sys.stdout.flush()
+    logf.close()
+    resultf.close()
+
